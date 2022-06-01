c4390258dd251b44a2d89722d735ebc06b476d66
laecio silva <laecio12silva@gmail.com>
2022-06-01 16:38:02 -0300

    feat: :zap: alterando estrura do db

--- db.sql
+++ db.sql
@@ -9,24 +9,34 @@ CREATE TABLE users (
   bio VARCHAR(255),
   PRIMARY KEY(id)
 ) ENGINE = INNODB;
+
 CREATE TABLE apis (
   id INT NOT NULL auto_increment,
   name VARCHAR(255) NOT NULL,
   url_repo VARCHAR(255) NOT NULL,
   technologies VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL
+  rating INT NOT NULL,
   user_id INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
   PRIMARY KEY(id)
 ) ENGINE = INNODB;
+
 CREATE TABLE front (
   id INT NOT NULL auto_increment,
   name VARCHAR(255) NOT NULL,
   url_repo VARCHAR(255) NOT NULL,
   technologies VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  rating INT NOT NULL,
   api_id INT NOT NULL,
+  api_category VARCHAR(255) NOT NULL,
   url_deploy VARCHAR(255),
   user_id INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
   FOREIGN KEY (api_id) REFERENCES apis (id) ON DELETE CASCADE,
+  FOREIGN KEY (api_category) REFERENCES apis (category) ON DELETE CASCADE,
   PRIMARY KEY(id)
 ) ENGINE = INNODB;
