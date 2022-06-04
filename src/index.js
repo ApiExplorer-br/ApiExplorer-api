22ea15290c8edd56befc1e537f2489a12419eafd
laecio silva <laecio12silva@gmail.com>
2022-06-04 08:03:42 -0300

    feat: :zap: Middleware de error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import 'express-async-errors';
 
 import 'dotenv/config';
+import { AppError } from './errors/AppError.js';
 import { router } from './routes/index.js';
 
 const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
+app.use((err, _request, response, next) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({ message: err.message });
+  }
+  if (!err.statusCode) {
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+  return next();
 });
+
+app.listen(3333, () => console.log('Server started on port 3333'));
