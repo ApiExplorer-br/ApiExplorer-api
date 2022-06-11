21387b0edf063823b5730046c3fb5a563ce62ab0
laecio silva <laecio12silva@gmail.com>
2022-06-11 13:49:14 -0300

    feat: :fire: criando uma avaliação

--- src/controllers/ratingControllers.js
+++ src/controllers/ratingControllers.js
@@ -1,9 +1,15 @@
 import { createRatingService } from '../services/ratingService.js';
 
-
 export const createRating = async (request, response) => {
-    const { rating, message, api_id } = request.body;
-    const { id: userId, name } = request.user;
-    const insertRating = await createRatingService(userId, api_id, rating, message, name);
-    return response.status(201).send(insertRating);
-}
+  const { rating, message, api_id } = request.body;
+  const { id: userId, name } = request.user;
+
+  const insertRating = await createRatingService(
+    userId,
+    api_id,
+    rating,
+    message,
+    name
+  );
+  return response.status(201).send(insertRating);
+};
--- src/models/RatingModel.js
+++ src/models/RatingModel.js
@@ -1,15 +1,24 @@
 import { connection } from '../db/index.js';
 
-
 export const getApiRatingById = async (id) => {
-    const [rating] = await connection.execute(`SELECT * FROM rating WHERE api_id = ?`, [id]);
-    return rating;
+  const [rating] = await connection.execute(
+    `SELECT * FROM rating WHERE api_id = ?`,
+    [id]
+  );
+  return rating;
 };
 
-export const createRatingModel = async (id, api_id, rating, message, userId, name) => {
-    const result = await connection.query(
-        `INSERT INTO rating (id, api_id, rating, message, userId, user_name) VALUES (?, ?, ?, ?, ?, ?)`,
-        [id, api_id, rating, message, userId, name]
-    );
-    return result;
+export const createRatingModel = async (
+  id,
+  api_id,
+  rating,
+  message,
+  userId,
+  name
+) => {
+  const result = await connection.query(
+    `INSERT INTO rating (id, api_id, rating, message, user_id, user_name) VALUES (?, ?, ?, ?, ?, ?)`,
+    [id, api_id, rating, message, userId, name]
+  );
+  return result;
 };
