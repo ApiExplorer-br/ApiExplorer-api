b2330524fb211a891d12b9754fefd7a468562761
laecio silva <laecio12silva@gmail.com>
2022-06-12 16:37:34 -0300

    feat: :sparkles: admin pode apagar um avaliação

--- src/middlewares/isOwnerRating.js
+++ src/middlewares/isOwnerRating.js
@@ -2,11 +2,11 @@ import { AppError } from '../errors/AppError.js';
 import { getRatingById } from '../models/RatingModel.js';
 
 export const isOwnerRating = async (req, _res, next) => {
-  const { id: userId } = req.user;
+  const { id: userId, admin } = req.user;
   const { id } = req.params;
   const [rating] = await getRatingById(id);
 
-  if (rating && rating.user_id !== userId)
+  if (rating && rating.user_id !== userId && !admin)
     throw new AppError('Unauthorized user', 401);
 
   next();
