a5c2428d669e381f6a5fbc4ee68eb339d49a3beb
laecio silva <laecio12silva@gmail.com>
2022-06-12 16:41:33 -0300

    feat: :sparkles: admin pode apagar uma api

--- src/middlewares/isOwnerApi.js
+++ src/middlewares/isOwnerApi.js
@@ -3,10 +3,10 @@ import { getApiByIdService } from '../services/apisService.js';
 
 export const isOwnerApi = async (request, _response, next) => {
   const { id } = request.params;
-  const { id: userId } = request.user;
+  const { id: userId, admin } = request.user;
   const api = await getApiByIdService(id);
 
-  if (api && api.user_id !== userId)
+  if (api && api.user_id !== userId && !admin)
     throw new AppError('Unauthorized user', 401);
 
   next();
