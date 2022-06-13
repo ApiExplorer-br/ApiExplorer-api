9fd5704d335c57e59002eabf7065c7087a9fe1dd
laecio silva <laecio12silva@gmail.com>
2022-06-13 15:37:06 -0300

    feat: :fire: Apenas o admin pode deletar uma api

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -7,8 +7,8 @@ import {
   getApiById,
   deleteApi,
 } from '../controllers/apiControllers.js';
+import { ensureAdmin } from '../middlewares/ensureAdmin.js';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js';
-import { ensureIsApiOwner } from '../middlewares/ensureIsApiOwner.js';
 import { validateUrl } from '../middlewares/validateUrl.js';
 
 const apiRouter = express.Router();
@@ -17,6 +17,6 @@ apiRouter.get('/', getAllApis);
 apiRouter.get('/:id', getApiById);
 apiRouter.get('/category/:id', getApiByCategory);
 apiRouter.post('/', ensureAuthenticated, validateUrl, createApi);
-apiRouter.delete('/:id', ensureAuthenticated, ensureIsApiOwner, deleteApi);
+apiRouter.delete('/:id', ensureAuthenticated, ensureAdmin, deleteApi);
 
 export { apiRouter };
