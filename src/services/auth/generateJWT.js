5235a36f0203ed08aad00e1ad67bc97ac67cbb18
laecio silva <laecio12silva@gmail.com>
2022-06-13 08:21:20 -0300

    feat: :zap: Melhorando a solução para gerar um token

--- src/services/auth/generateJWT.js
+++ src/services/auth/generateJWT.js
@@ -1,10 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-import { getByEmail } from '../../models/UserModel.js';
-
-export const createJwt = async (email) => {
-  const [user] = await getByEmail(email);
-
+export const generateJWT = async (user) => {
   const jwtConfig = { expiresIn: '48h', algorithm: 'HS256' };
 
   const secret = process.env.JWT_SECRET;
