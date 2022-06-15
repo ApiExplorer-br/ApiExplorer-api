c469d5459167d8635098b755a8b8c677d8e577c7
laecio silva <laecio12silva@gmail.com>
2022-06-15 15:26:39 -0300

    fix:

--- src/middlewares/validateData.js
+++ src/middlewares/validateData.js
@@ -3,14 +3,14 @@ import { AppError } from '../errors/AppError.js';
 const validateData = (req, res, next) => {
   const { url, description } = req.body;
 
-  if (!url) throw new AppError('Url é obrigatória', 400);
+  if (!url) throw new AppError('Url é obrigatória!', 400);
 
-  if (!description) throw new AppError('Descrição é obrigatória');
+  if (!description) throw new AppError('Descrição é obrigatória!');
 
-  if (description.length > 255) throw new AppError('Descrição muito longa');
+  if (description.length > 255) throw new AppError('Descrição muito longa!');
 
   if (!url.startsWith('https://github.com/'))
-    throw new AppError('Url inválida');
+    throw new AppError('Url inválida!');
 
   next();
 };
