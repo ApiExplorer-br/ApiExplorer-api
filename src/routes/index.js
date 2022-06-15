382ed8b0ad7d6e477251e798239cc66097605573
paolofullone <paolo.enrico@gmail.com>
2022-06-15 11:28:41 -0300

    feat(Routes): :sparkles: index routes to include front

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import { categoryRouter } from './categoryRouter.js';
 import { feedbackRouter } from './feedbackRouter.js';
 import { ratingRouter } from './ratingRouter.js';
 import { userRouter } from './userRouter.js';
+import { frontRouter } from './frontRouter.js';
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use('/users', userRouter);
 router.use('/categories', categoryRouter);
 router.use('/ratings', ratingRouter);
 router.use('/feedbacks', feedbackRouter);
+router.use('/fronts', frontRouter);
 
 export { router };
