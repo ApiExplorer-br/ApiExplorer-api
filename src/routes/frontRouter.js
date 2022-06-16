5a65af1ce0f78a374db28b43903077942c51179c
paolofullone <paolo.enrico@gmail.com>
2022-06-16 19:18:07 -0300

    feat: :sparkles: front router

--- src/routes/frontRouter.js
+++ src/routes/frontRouter.js
@@ -3,20 +3,22 @@ import express from 'express';
 import {
     createFront,
     getAllFronts,
-    getFrontById
+    getFrontById,
+    editFront    
 } from '../controllers/frontController.js';
 
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js';
-// import { ensureIsRatingOwner } from '../middlewares/ensureIsRatingOwner.js'; 
-// ajustare para ensureIsFrontOwner
-
+import { validateFront } from '../middlewares/validateFront.js';
+import { ensureIsFrontOwner } from '../middlewares/ensureIsFrontOwner.js'; 
 
 const frontRouter = express.Router();
 
 frontRouter.post('/', ensureAuthenticated, createFront);
 frontRouter.get('/', getAllFronts);
 frontRouter.get('/:id', getFrontById);
+frontRouter.put('/:id', ensureAuthenticated, ensureIsFrontOwner, validateFront, editFront);
+
 
 
 
