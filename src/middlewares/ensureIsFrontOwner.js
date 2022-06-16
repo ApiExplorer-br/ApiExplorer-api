3c7807db9ff8102bb65b73a4ccb276352be27eb4
paolofullone <paolo.enrico@gmail.com>
2022-06-16 19:09:58 -0300

    feat: :sparkles: ensureIsFrontOwner

---
+++ src/middlewares/ensureIsFrontOwner.js
@@ -0,0 +1,13 @@
+import { AppError } from '../errors/AppError.js';
+import { getFrontByIdService } from '../services/frontService.js';
+
+export const ensureIsFrontOwner = async (request, _response, next) => {
+    const { id } = request.params;
+    const { id: user_id, admin } = request.user;
+    const [front] = await getFrontByIdService(id);
+
+    if (front.user_id !== user_id && !admin)
+        throw new AppError('Usuário não autorizado a atualizar este front.', 401);
+    
+    next();
+}
