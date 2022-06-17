98c296088300daf9695f64fd8f42a7c3e6dc836b
paolofullone <paolo.enrico@gmail.com>
2022-06-17 18:43:39 -0300

    fix: :bug: middleware de validação de front

--- src/middlewares/validateFront.js
+++ src/middlewares/validateFront.js
@@ -3,8 +3,13 @@ import { AppError } from '../errors/AppError.js';
 const validateFront = (req, _res, next) => {
   const { url, category, api_id, description } = req.body;
 
-  if (!api_id && !category && !description && !url)
-    throw new AppError('Algum campo deve ser informado para atualização!');
+  if (!url) throw new AppError('Url é obrigatória!');
+  if (!url.startsWith('https://github.com/'))
+    throw new AppError('A url precisa ser de um repositório do github!');
+
+  if (!category) throw new AppError('Categoria é obrigatória!');
+  if (!api_id) throw new AppError('Api é obrigatória!');
+  if (!description) throw new AppError('Descrição é obrigatória!');
 
   if (description.length > 255)
     throw new AppError('Nome ou descrição muito longo!');
