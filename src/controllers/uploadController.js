913dfe584cde5518f62892d4e621c21d7df0a1e4
laecio silva <laecio12silva@gmail.com>
2022-06-22 08:12:03 -0300

    feat: :sparkles: Criando o controller responsável por deletar uma imagem na AWS.

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,7 +1,9 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import { deleteFileFromS3 } from '../config/libs/s3Client.js';
 import { addImageFrontService } from '../services/frontService.js';
+
 // disable eslint for this file
 /* eslint-disable */
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +12,12 @@ const __dirname = path.dirname(__filename);
 export const addImage = async (request, response) => {
   const { file } = request;
   const { id } = request.params;
-
   await addImageFrontService(file.location, id);
   response.status(200).json(file);
 };
+
+export const deleteImage = async (request, response) => {
+  const { Key } = request.params;
+  const message  = await deleteFileFromS3(Key);
+  response.status(200).json({ message });
+} 
