dc992e56181d3239a56a72df02e66d5be7716789
laecio silva <laecio12silva@gmail.com>
2022-06-22 08:08:48 -0300

    feat: :sparkles: Criando a rota para deletar uma imagem na AWS e alterando a rota de adicionar pata  image/:id

--- src/routes/frontRouter.js
+++ src/routes/frontRouter.js
@@ -9,7 +9,7 @@ import {
   editFront,
   deleteFront,
 } from '../controllers/frontController.js';
-import { addImage } from '../controllers/uploadController.js';
+import { addImage, deleteImage } from '../controllers/uploadController.js';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated.js';
 import { ensureIsFrontOwner } from '../middlewares/ensureIsFrontOwner.js';
 import { validateEditFront } from '../middlewares/validateEditFront.js';
@@ -33,6 +33,7 @@ frontRouter.delete(
   ensureIsFrontOwner,
   deleteFront
 );
-frontRouter.post('/upload/:id', muter(multerConfig).single('file'), addImage);
+frontRouter.put('/image/:id', muter(multerConfig).single('file'), addImage);
+frontRouter.delete('/image/:Key', deleteImage);
 
 export { frontRouter };
